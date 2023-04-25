c8ec83d014221aa0a1c6841c74598377276a697a
Nicholas Schuetze <nichschuetze@gmail.com>
2023-04-25 00:45:52 -0500

    Added overflow to Daily

--- src/components/tenDayForecast.tsx
+++ src/components/tenDayForecast.tsx
@@ -2,9 +2,9 @@ import useForecast from "@/services/useForecast";
 
 const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
-function DailyForecast({ forecast }: {forecast: any}) {
+function DailyForecast({forecast}: { forecast: any }) {
     return (
-        <div className={"basis-full p-2 hover:border-2 active:bg-gray-400"}>
+        <div className={"w-36 flex-shrink-0 p-2 hover:border-2 active:bg-gray-400"}>
             <p>{forecast.name}</p>
             <p>☁️</p>
             <p className={"break-normal"}>{forecast.temperature}°</p>
@@ -12,19 +12,20 @@ function DailyForecast({ forecast }: {forecast: any}) {
         </div>
     );
 }
+
 export default function TenDayForecast() {
     const {forecast, isLoading, isError} = useForecast()
     if (isError) return <div>failed to load</div>
     if (isLoading) return <div>loading...</div>
-    const futureForecast = forecast.properties.periods.filter((period:any) => {
+    const futureForecast = forecast.properties.periods.filter((period: any) => {
         return days.includes(period.name)
     })
 
     return (
-        <div className={"flex flex-col max-w-screen-lg"}>
+        <div className={"flex flex-col max-w-screen-lg w-screen"}>
             <h2 className={"pl-2"} style={{paddingLeft: "10px"}}>Daily</h2>
-            <div className={"flex flex-row"}>
-                {futureForecast.map( (dayForecast:any) => {
+            <div className={"flex overflow-x-auto"}>
+                {futureForecast.map((dayForecast: any) => {
                     return <DailyForecast key={dayForecast.number} forecast={dayForecast}/>
                 })}
             </div>
